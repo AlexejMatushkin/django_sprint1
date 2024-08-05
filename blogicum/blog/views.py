40e10c7ba9e91449fba2dc57a3880e3087fb6344
Alexej Matushkin <matushkin.leha2018@gmail.com>
2024-08-06 00:26:40 +0300

    flake8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -57,6 +57,8 @@ def post_detail(request, post_id):
 
 
 def category_posts(request, category_slug):
-    filtered_posts = [post for post in posts if post['category'] == category_slug]
+    filtered_posts = [
+        post for post in posts if post['category'] == category_slug
+    ]
     context = {'posts': filtered_posts, 'category': category_slug}
     return render(request, 'blog/category.html', context)
